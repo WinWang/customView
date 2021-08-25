1a2762a5fbbd1395dc445dc062657135e73da27f
WinWang <1256242870@qq.com>
2021-08-25 20:28:58 +0800

    升级gradle版本

---
+++ app/src/main/java/com/winwang/myapplication/activity/RvHeaderActivity.kt
@@ -0,0 +1,12 @@
+package com.winwang.myapplication.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.winwang.myapplication.R
+
+class RvHeaderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rv_header)
+    }
+}
--- app/src/main/java/com/winwang/myapplication/activity/VerticalProgressActivity.kt
+++ app/src/main/java/com/winwang/myapplication/activity/VerticalProgressActivity.kt
@@ -2,11 +2,23 @@ package com.winwang.myapplication.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.winwang.myapplication.R
+import kotlinx.coroutines.delay
 
 class VerticalProgressActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vertical_progress)
+
+        lifecycleScope.launchWhenResumed {
+            when (true) {
+                true -> {
+                    delay(5000)
+                    Log.e("测试输出", "1111111111")
+                }
+            }
+        }
     }
 }
